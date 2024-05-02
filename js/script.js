131d6ab20ac0f44ad3f17031c7b2c613c2ab0fa4
Ignacio Cometti <ignaciogcometti@gmail.com>
2024-05-02 17:49:22 -0300

    agregar music and h1

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ const constancia = " para mantener la disciplina";
 
 let estudio = practicar + constancia;
 console.log(estudio);
+
+<script>
+  window.addEventListener("load", function() {
+    document.getElementById("cancion1").play();
+    document.getElementById("cancion2").play();
+    document.getElementById("cancion3").play();
+    document.getElementById("cancion4").play();
+    document.getElementById("cancion5").play();
+    // Reproduce todas las canciones de tu lista aquí
+  });
+</script>
