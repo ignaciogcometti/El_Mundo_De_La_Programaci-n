49713ce62b774d9b404283ffd650cfda7d3d51d2
Ignacio Cometti <ignaciogcometti@gmail.com>
2024-05-31 18:16:11 -0300

    ajustar navbar fixed-top

---
+++ js/carousel.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#carouselExampleIndicators').carousel({
+        interval: 2000, // Cambia de imagen cada 2 segundos
+        ride: 'carousel'
+    });
+});
--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 document.getElementById('enterButton').addEventListener('click', function() {
   window.location.href = "../pages/realpage.html"; // Reemplaza 'page2.html' con la URL de la verdadera página
 });
+
+function changeColorAndGoBack(element) {
+        element.classList.add("selected"); // Agrega la clase "selected" al logo para cambiar su color
+        window.history.back(); // Navega hacia atrás en la historia del navegador
+}
